55aacb8c06e51914d714ea8a65889cd635b316ba
kmbarnett <kmbarnett94@gmail.com>
2020-07-23 16:22:31 -0600

    Change structure

--- src/Colors.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Colors({ hex }) {
-  return (
-    <div style={{ backgroundColor: hex }} className="color">
-      <div className="hex-value">{hex}</div>
-    </div>
-  );
-}
