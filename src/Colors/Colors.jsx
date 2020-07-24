5ba14aff235b384c78ea44310b6a440f7e85fb6b
kmbarnett <kmbarnett94@gmail.com>
2020-07-24 14:31:29 -0600

    Add ToolTip

--- src/Colors/Colors.jsx
+++ src/Colors/Colors.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import ReactTooltip from "react-tooltip";
 
 export default function Colors({ hex, complement }) {
   const [copied, setCopied] = useState(false)
@@ -7,14 +8,17 @@ export default function Colors({ hex, complement }) {
   const handleCopy = () => {
     setCopied(true)
     setTimeout(setCopied, 1000, false);
-  }
+  } 
+
 
   return (
     <CopyToClipboard onCopy={handleCopy} text={hex}>
       <div
         style={{ backgroundColor: hex, color: complement }}
         className="color"
+        data-tip="Click to Copy"
       >
+        <ReactTooltip />
         <div style={{ color: complement }} className="hex-value">
           <p className={`copied-${copied} copied`}>Copied</p>
           <button style={{ color: complement }} className="hex-copy">
