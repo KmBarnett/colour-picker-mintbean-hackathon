18ca5b644776f90db3482517956f0806971e94de
kmbarnett <kmbarnett94@gmail.com>
2020-07-23 20:49:16 -0600

    Make color cards more functional

--- src/Colors/Colors.jsx
+++ src/Colors/Colors.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function Colors({ hex, complement }) {
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    setCopied(true)
+    setTimeout(setCopied, 1000, false);
+  }
+
   return (
-    <div style={{ backgroundColor: hex, color: complement }} className="color">
-      <div className="hex-value">{hex}</div>
-    </div>
+    <CopyToClipboard onCopy={handleCopy} text={hex}>
+      <div
+        style={{ backgroundColor: hex, color: complement }}
+        className="color"
+      >
+        <div style={{ color: complement }} className="hex-value">
+          <p className={`copied-${copied} copied`}>Copied</p>
+          <button style={{ color: complement }} className="hex-copy">
+            {hex}
+          </button>
+        </div>
+      </div>
+    </CopyToClipboard>
   );
 }
